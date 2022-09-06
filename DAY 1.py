9c0e4a39452db4224deb6adcbb0ed16c63961fb8
ALcoderjiya <106988119+ALcoderjiya@users.noreply.github.com>
2022-09-06 18:48:34 +0530

    Add files via upload

---
+++ DAY 1.py
@@ -0,0 +1,7 @@
+#DAY 1 print your introduction
+print("-"*30)
+print("My name is Jiya Choudhary")
+print("I am a 12th Standard Student")
+print("I love Coding")
+print("Thanks for watching")
+print("-"*30)
