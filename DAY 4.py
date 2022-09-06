411d0622097f2631912fcbfd151b7096cd573b3f
ALcoderjiya <106988119+ALcoderjiya@users.noreply.github.com>
2022-09-06 18:52:51 +0530

    Add files via upload

---
+++ DAY 4.py
@@ -0,0 +1,19 @@
+# DAY 4 shopping bill with discounts
+name = input("Enter Customer Name : ")
+cost = float(input("Enter cost of the product : "))
+qty = float(input("Enter quantity of the product : "))
+amount = cost * qty
+gst = 12 * amount / 100
+total = amount + gst
+
+if  total < 1000:
+   discount = 0
+elif total < 3000:
+   discount = 0.1 * total
+else:
+   discount = 0.2 * total
+
+print("Amount is ",amount)
+print("GST is ",gst)
+print("Discount is ",discount)
+print("Amount ot be paid ",total - discount)
