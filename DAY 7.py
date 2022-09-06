fd57e43e0bf9ce49d0d03fe5c890e9242e3661dc
ALcoderjiya <106988119+ALcoderjiya@users.noreply.github.com>
2022-09-06 18:55:23 +0530

    Add files via upload

---
+++ DAY 7.py
@@ -0,0 +1,12 @@
+# DAY 7 leap year or not
+year = int(input("Enter the year to be checked: "))
+if year & 100 == 0:
+    if year % 400 == 0:
+        print(year, "  is a Leap Year")
+    else:
+        print(year, " is not a leap year")
+else:
+    if year % 4 == 0:
+        print(year, " is a Leap year")
+    else:
+        print(year, " is not a Leap year")
