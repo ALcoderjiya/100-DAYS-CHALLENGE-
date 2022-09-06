9a8c9143a227a73cdd792966b4250b669a9258c0
ALcoderjiya <106988119+ALcoderjiya@users.noreply.github.com>
2022-09-06 18:51:15 +0530

    Add files via upload

---
+++ DAY 2.py
@@ -0,0 +1,6 @@
+#DAY 2 AREA & CIRCUMFERENCE OF CIRCLE
+radius = float(inpuy("Enter the radius of the circle : "))
+area = 3.14 * radius ** 2
+cir = 2 * 3.14 * radius
+print("Area of Circle is ",round(area,2),"sq cm")
+print("Circumference is ",round(cir,2),"cm")
