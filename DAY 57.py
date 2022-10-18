594f6bdfda62a7d1fba1eb539971048a344614d5
ALCODERJIYA <106988119+ALcoderjiya@users.noreply.github.com>
2022-10-18 14:30:21 +0530

    Add files via upload

---
+++ DAY 57.py
@@ -0,0 +1,13 @@
+#DAY 57 Validate Name Entered by User
+def isValidName(name):
+    for ch in name:
+        if not (ch.isalpha() or ch == ' '):
+            return False
+    else:
+        return True
+
+txt = input("Enter the name to validate : ")
+if isValidName(txt):
+    print(txt + " is a valid name")
+else:
+    print(txt + " is not a valid name ")
