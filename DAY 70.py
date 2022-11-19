1b21eebf16b5aaaffa7564d1e30eb968a5997d0c
ALCODERJIYA <106988119+ALcoderjiya@users.noreply.github.com>
2022-11-19 10:39:14 +0530

    Add files via upload

---
+++ DAY 70.py
@@ -0,0 +1,12 @@
+#DAY 70 SUM OF DIGITS USING RECURSION
+def sumofdigit(num):
+    if num == 0:
+        return 0
+    d = num % 10
+    num = num // 10
+    res = d + sumofdigit(num)
+    return res
+
+n = int(input("Enter a number : "))
+r = sumofdigit(n)
+print("Sum of Digits of ",n," is ",r)
